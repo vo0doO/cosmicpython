e971b2d78238d5eac483e64e6732e843ac3c26f7
vo0doo <kirsanov.bvt@gmail.com>
2025-02-25 22:22:18 +0300

    update repository.py - exercise 02 completed

--- repository.py
+++ repository.py
@@ -1,4 +1,5 @@
 import abc
+
 import model
 
 
@@ -17,9 +18,60 @@ class SqlRepository(AbstractRepository):
         self.session = session
 
     def add(self, batch):
-        # self.session.execute('INSERT INTO ??
-        ...
+        result = self.session.execute(
+            "INSERT INTO batches (reference, sku, _purchased_quantity, eta)"
+            "VALUES (:ref, :sku, :qty, :eta)"
+            "RETURNING id",
+            dict(
+                ref=batch.reference,
+                sku=batch.sku,
+                qty=batch._purchased_quantity,
+                eta=batch.eta,
+            ),
+        )
+
+        batch_id = result.scalar()
+
+        if allocations := batch._allocations:
+            for orderline in allocations:
+                result = self.session.execute(
+                    "INSERT INTO order_lines (sku, qty, orderid)"
+                    "VALUES (:sku, :qty, :orderid)"
+                    "RETURNING id",
+                    dict(
+                        sku=orderline.sku, qty=orderline.qty, orderid=orderline.orderid
+                    ),
+                )
+
+                orderline_id = result.scalar()
+
+                self.session.execute(
+                    "INSERT INTO allocations (orderline_id, batch_id)"
+                    "VALUES (:orderline_id, :batch_id)",
+                    dict(orderline_id=orderline_id, batch_id=batch_id),
+                )
 
     def get(self, reference) -> model.Batch:
-        # self.session.execute('SELECT ??
-        ...
+
+        batch = model.Batch(
+            *self.session.execute(
+                "SELECT * FROM batches WHERE reference=:ref", dict(ref=reference)
+            ).first()[1:]
+        )
+        batch._allocations = self.get_allocations(reference)
+        return batch
+
+    def get_allocations(self, batchid):
+        query = """
+        SELECT 
+        order_lines.orderid, 
+        order_lines.sku,  
+        order_lines.qty 
+        FROM allocations
+        JOIN order_lines ON allocations.orderline_id = order_lines.id
+        JOIN batches ON allocations.batch_id = batches.id
+        WHERE batches.reference = :batchid;
+        """  # noqa: F841
+
+        rows = list(self.session.execute(query, dict(batchid=batchid)))
+        return set(model.OrderLine(*row) for row in rows)
